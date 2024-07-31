6db1683150fa4fa4575a55e0e65527f74ebb944d
Peter Li <lihaohong6@gmail.com>
2024-07-31 02:56:35 -0400

    strinova comms: new approach for determining conversation group

--- strinova_comms.py
+++ strinova_comms.py
@@ -1,4 +1,5 @@
 import dataclasses
+from itertools import takewhile
 
 from pywikibot import Page
 
@@ -12,33 +13,60 @@ group_counter = 1
 num = -1
 
 
+# FIXME: implement topological sort or something similar to deal with out-of-order options
 @dataclasses.dataclass
 class Node:
     node_id: str
     value: dict
-    in_degree: int = 0
+    next: list[str] = dataclasses.field(default_factory=list)
+    group: int | None = None
+    options: list[tuple[int, int]] = dataclasses.field(default_factory=list)
 
 
-def parse_simple_content(value):
-    # check to see if this is an emoji
-    if 'SourceString' not in value['TextContent']:
-        image_size = 160
-        text_image = value["TextureContent"]['AssetPathName']
-        emote = re.search(r"Emote_([0-9]+)", text_image)
-        if emote:
-            text_image = "表情 " + emote.group(1) + ".png"
-        id_card = re.search(r"IdCard_([0-9]+)", text_image)
-        if id_card:
-            text_image = "基板 " + id_card.group(1) + ".png"
-        chat_share = re.search(r"ChatShare_([0-9]+)", text_image)
-        if chat_share:
-            text_image = "聊天图 " + chat_share.group(1) + ".png"
-            image_size = 300
-        return f"[[File:{text_image}|{image_size}px]]"
-    else:
-        is_player = value['bIsPlayer']
-        text_class = "mc-text" if is_player else "char-text"
-        return '<span class{{=}}"' + text_class + '">' + value['TextContent']['SourceString'] + "</span>"
+def find_convergence_point(conversations: dict[str, Node], start: Node) -> str | None:
+    """
+    When dialogue branches based on navigator's choice, find out at which point the conversation
+    converges to the same lines and then modify Node::groups to match it.
+
+    Args:
+        conversations (list[dict]): all lines in this conversation
+        start (dict): the node at which the conversation diverges
+    """
+    global group_counter
+    current_group = group_counter
+    group_counter += 1
+    start.group = current_group
+    paths: list[list[str]] = []
+    for option_index, option in enumerate(start.next):
+        paths.append([])
+        paths[-1].append(option)
+        while option in conversations and option != "End":
+            next_key = conversations[option].next[0]
+            if next_key not in conversations:
+                return option
+            next_node = conversations[next_key]
+            paths[-1].append(next_node.node_id)
+            option = next_node.node_id
+    path_sets: list[set[str]] = [set(p for p in path) for path in paths]
+    convergence = -1
+    for cid in paths[0]:
+        for path_set in path_sets:
+            if cid not in path_set:
+                break
+        else:
+            convergence = cid
+            break
+    assert convergence != -1, str(start)
+
+    paths = [list(takewhile(lambda x: x != convergence, path)) for path in paths]
+    for index, path in enumerate(paths):
+        for key in path:
+            node = conversations[key]
+            if len(node.next) > 1:
+                assert find_convergence_point(conversations, node) is None
+            node.options.append((current_group, index + 1))
+
+    return None
 
 
 def get_i18n():
@@ -46,47 +74,33 @@ def get_i18n():
 
 
 def process_file(p: Path) -> str:
-    global group_counter
     i18n = get_i18n()
     obj = json.load(open(p, "r", encoding="utf-8"))[0]['Rows']
-    choice_flag = False
-    choice_counter = 1
 
     result = ["{{StrinovaComms"]
 
     nodes: dict[str, Node] = {}
-    prev: Node | None = None
     for key in obj:
         value = obj[key]
-        options_count = len(value['TextContentList']) // 2 + 1
+        next_nodes = [t for t in value['OptionalJumpRowNameArray'] if t != ""]
+        normal_jump_row_name = value['NormalJumpRowName']
+        if len(next_nodes) == 1 and normal_jump_row_name != "":
+            print(f"Ambiguous jump in {key} of {p.name}. Guessing between {next_nodes[0]} and {normal_jump_row_name}")
+            if normal_jump_row_name == "End" or int(normal_jump_row_name) > int(next_nodes[0]):
+                next_nodes = [normal_jump_row_name]
+        elif len(next_nodes) == 0:
+            next_nodes = [normal_jump_row_name]
+        assert len(next_nodes) > 0
         nodes[key] = Node(key,
-                          value)
-        if prev is not None:
-            prev.next_id = key
-        prev = nodes[key]
+                          value,
+                          next=next_nodes)
 
-    for key, value in obj.items():
-        jump_row = value["NormalJumpRowName"]
-        jump_row_array = value["OptionalJumpRowNameArray"]
-        if jump_row != "":
-            if jump_row in nodes:
-                nodes[jump_row].in_degree += 1
-            else:
-                print(f"Invalid jump target of {jump_row} in {p.name}")
-        elif len(jump_row_array) >= 2:
-            for target in jump_row_array:
-                nodes[target].in_degree += 1
-        elif len(jump_row_array) == 1:
-            target = jump_row_array[0]
-            if target in nodes:
-                nodes[target].in_degree += 2
-            else:
-                print(f"Invalid jump target of {target} in {p.name}")
-        elif key == "End":
-            # sometimes there are mysterious rows after End
-            break
-        elif key != "Start":
-            raise RuntimeError(f"{p}: {key}, {value}")
+    for key, value in nodes.items():
+        if len(value.next) > 1:
+            ret = find_convergence_point(nodes, value)
+            if ret is not None:
+                print(f"Convergence point not found in {p.name}. Reason is: {ret}.")
+                return ""
 
     line_counter = 1
     for key, node in nodes.items():
@@ -102,9 +116,11 @@ def process_file(p: Path) -> str:
         char_name = char_id_mapper.get(from_id)
         profile_str = f'\n|profile{line_counter}={char_name} Profile\n|name{line_counter}={char_name}' if not is_player and char_name else ""
         choice_string = ""
-        if choice_flag:
-            choice_string = f"\n|group{line_counter}={group_counter}\n" \
-                            f"|option{line_counter}={choice_counter}"
+        if len(node.options) > 0:
+            assert len(node.options) == 1
+            group, option = node.options[0]
+            choice_string = f"\n|group{line_counter}={group}\n" \
+                            f"|option{line_counter}={option}"
         if len(text) == 0:
             text = [value['TextContent']]
         new_text = []
@@ -113,13 +129,13 @@ def process_file(p: Path) -> str:
         text = new_text
 
         if len(text) > 1:
-            group_counter += 1
-            line = f"|{line_counter}=reply\n" \
-                   f"|group{line_counter}={group_counter}\n" + \
-                   "\n".join(f"|option{line_counter}_{index + 1}={t}" for index, t in enumerate(text))
+            assert is_player
+            # note that node.next may have length 1 because both options lead to the same result
+            line = (f"|{line_counter}=reply\n" +
+                    f"|group_start{line_counter}={node.group}\n" +
+                    "\n".join(f"|option{line_counter}_{index + 1}={t}" for index, t in enumerate(text)) +
+                    choice_string)
             result.append(line)
-            choice_flag = True
-            choice_counter = 1
             line_counter += 1
         # no text in content list, either emoji or single line response
         elif "Voice" in value['ContentType']:
@@ -150,16 +166,6 @@ def process_file(p: Path) -> str:
             line_counter += 1
             result.append(line)
 
-        if len(value['OptionalJumpRowNameArray']) > 0:
-            next_node = value['OptionalJumpRowNameArray'][0]
-        else:
-            next_node = value["NormalJumpRowName"]
-        if node.next_id != next_node:
-            # skipping ahead: transitioning from option 1 to option 2
-            choice_counter += 1
-        elif node.in_degree >= 2 or nodes[next_node].in_degree >= 2:
-            # about to merge into the same conversation
-            choice_flag = False
     result.append("}}")
     return "\n\n".join(result)
 
@@ -168,7 +174,7 @@ def main():
     # TODO: KaChatOption.json has character favorability boosts
     ka_phone_root = csv_root.joinpath("KaPhone")
     name_mapper = {
-        'HuiXing': 'Celestia'
+        'Huixing': 'Celestia'
     }
     skip = {
         "Fuchsia"
@@ -181,27 +187,31 @@ def main():
         conversation_name = name_mapper.get(conversation_name, conversation_name)
         if conversation_name not in char_id_mapper.values() or conversation_name in skip:
             continue
-        tabs = []
-        contents = []
+        # tab name, tab content, and sort weight; smaller is more important
+        x: list[tuple[str, str, int]] = []
         for file in parent.glob("*.json"):
             name = file.name.capitalize()
             tab = "?"
-            last_segment = name.split("_")[-1].split(".")[0]
-            if name.startswith(conversation_name):
-                tab = "Friendship Lv. " + last_segment
+            last_segment = re.search(r"\d+$", name.split("_")[-1].split(".")[0]).group(0)
+            if name.startswith(conversation_name) or name.startswith(parent.name.capitalize()):
+                tab = ("Friendship Lv. " + last_segment, 0 + int(last_segment))
             elif name.lower().startswith("playerbirthday"):
-                tab = "Player birthday " + last_segment
+                tab = ("Player birthday " + last_segment, 20 + int(last_segment))
             elif name.lower().startswith("birthday"):
-                tab = f"{conversation_name} birthday " + last_segment
-            tabs.append(tab)
-            contents.append(process_file(file))
+                tab = (f"{conversation_name} birthday " + last_segment, 10 + int(last_segment))
+            processed = process_file(file)
+            if processed != "":
+                x.append((tab[0], processed, tab[1]))
         group_string = f" group=strinova_comms_{conversation_name} | "
+        tabs, contents = zip(*[(t[0], t[1]) for t in sorted(x, key=lambda t: t[2])])
         result = "{{Tab/tabs| " + group_string + " | ".join(tabs) + " }}\n" + \
-                 "{{Tab/content| " + group_string + "\n\n" + "\n\n|\n\n".join(contents) + "\n\n}}"
+                 "{{Tab/content| " + group_string + "\n\n" + "\n\n|\n\n".join(contents) + "\n\n}}" + \
+                 "<noinclude>[[Category:Strinova Comms]]</noinclude>"
 
         p = Page(s, conversation_name + "/Strinova Comms")
-        p.text = result
-        p.save(summary="generate strinova comms")
+        if p.text.strip() != result:
+            p.text = result
+            p.save(summary="generate strinova comms")
         global group_counter
         group_counter = 1
 
