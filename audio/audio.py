814bbc9d45b645c710f4be694db1b832ea09aab9
Peter Li <lihaohong6@gmail.com>
2024-08-26 02:26:39 -0400

    refactor audio and add pull from miraheze utility

--- audio/audio.py
+++ audio/audio.py
@@ -3,21 +3,24 @@ import re
 import json
 import os
 import subprocess
-from dataclasses import dataclass, fields, field
-from enum import Enum
 from pathlib import Path
+from sys import argv
 from typing import Any
 
 from pywikibot import FilePage, Page
 from pywikibot.pagegenerators import PreloadingGenerator, GeneratorFactory
 
+from audio.audio_parser import VoiceUpgrade, Voice, Trigger, in_game_triggers, in_game_triggers_upgrade, role_voice, \
+    match_custom_triggers
 from data.conversion_table import VoiceType
 from global_config import char_id_mapper, internal_names
-from utils.asset_utils import audio_root, audio_event_root, wav_root_cn, wav_root_jp
+from utils.asset_utils import audio_root, wav_root_cn
 from data.conversion_table import voice_conversion_table
-from utils.general_utils import get_table, get_game_json, get_bwiki_char_pages, load_json
+from utils.general_utils import get_bwiki_char_pages, load_json
 from utils.uploader import upload_file
 from utils.wiki_utils import s, bwiki
+
+import wikitextparser as wtp
 
 
 def audio_convert():
@@ -29,272 +32,6 @@ def audio_convert():
         out_path.parent.mkdir(parents=True, exist_ok=True)
         out_path = out_path.parent.joinpath(file_name.replace(".txtp", ".wav"))
         subprocess.call(["vgmstream-cli.exe", file, "-o", out_path], stdout=open(os.devnull, 'wb'))
-
-
-class VoiceUpgrade(Enum):
-    REGULAR = ""
-    ORG = "org"
-    RED = "red"
-
-
-@dataclass
-class Voice:
-    id: list[int]
-    role_id: int = -1
-    quality: int = -1
-    name_cn: str = ""
-    name_en: str = ""
-    text_cn: str = ""
-    text_en: str = ""
-    text_jp: str = ""
-    path: str = ""
-    file_cn: str = ""
-    file_jp: str = ""
-    file_page_cn: str = ""
-    file_page_jp: str = ""
-    upgrade: VoiceUpgrade = VoiceUpgrade.REGULAR
-
-    def merge(self, o: "Voice"):
-        assert self.file_cn == o.file_cn and self.path == o.path
-        assert len(o.id) == 1
-        self.id.append(o.id[0])
-        for f in fields(self):
-            if f.type == str:
-                a1 = getattr(self, f.name)
-                a2 = getattr(o, f.name)
-                if a1 == a2:
-                    continue
-                if a1 == "":
-                    setattr(self, f.name, a2)
-                raise RuntimeError(str(self) + "\n" + str(o))
-        if self.role_id != o.role_id:
-            if self.role_id == 999:
-                self.role_id = o.role_id
-
-
-@dataclass
-class Trigger:
-    id: int
-    type: VoiceType
-    name_cn: str = ""
-    name_en: str = ""
-    description_cn: str = ""
-    description_en: str = ""
-    voice_id: list[int] = field(default_factory=list)
-    # 0: applicable for all
-    # otherwise: applicable to a single character
-    role_id: int = 0
-    voices: list[Voice] = field(default_factory=list)
-    children: list["UpgradeTrigger"] = field(default_factory=list)
-
-    def merge(self, o: "Trigger"):
-        assert self.id == o.id and self.type == o.type
-        for f in fields(self):
-            if f.type == str:
-                a1 = getattr(self, f.name)
-                a2 = getattr(o, f.name)
-                if a1 == a2:
-                    continue
-                if a1 == "":
-                    setattr(self, f.name, a2)
-                raise RuntimeError(str(self) + "\n" + str(o))
-        if self.role_id != o.role_id:
-            if self.role_id == 999:
-                self.role_id = o.role_id
-
-
-@dataclass
-class UpgradeTrigger:
-    trigger: int
-    voice_id: list[int]
-    skins: list[int]
-
-
-def find_audio_file(file_name: str, table: dict, bank_name_to_files: dict) -> str | None:
-    assert len(table) > 0
-    event_file = audio_event_root / f"{file_name}.json"
-    if not event_file.exists():
-        # print(event_file.name + " does not exist")
-        return None
-    data = load_json(event_file)["Properties"]
-    bank_name = data["RequiredBank"]["ObjectName"].split("'")[1]
-    short_id = str(data["ShortID"])
-    if short_id not in table:
-        # print(f"Short ID {short_id} is not in conversion table")
-        return None
-    ix = int(table[short_id])
-    candidates = []
-    if bank_name not in bank_name_to_files:
-        # print(f"No file corresponding to bank name " + bank_name)
-        return None
-    for f in bank_name_to_files[bank_name]:
-        if f"-{ix:04d}-" in f.name:
-            candidates.append(f)
-    if len(candidates) == 0:
-        # print(f"No audio file found for {file_name} and bank {bank_name}")
-        return None
-    return candidates[0].name
-
-
-def parse_bank(bank_file: Path, table: dict[str, str]):
-    assert bank_file.exists()
-    lines = open(bank_file, "r", encoding="utf-8").readlines()
-    ix = None
-    sid = None
-    for line in lines:
-        if "ix=" in line:
-            ix = re.search(r'ix="(\d+)"', line).group(1)
-        if 'ty="sid"' in line:
-            sid = re.search(r'va="(\d+)"', line).group(1)
-            if ix is not None:
-                table[sid] = ix
-
-
-def parse_banks_xml():
-    sid_to_ix_cn = {}
-    cn_bank_file = audio_root / "banks/cn_banks.xml"
-    parse_bank(cn_bank_file, sid_to_ix_cn)
-    sid_to_ix_ja = {}
-    jp_bank_file = audio_root / "banks/ja_banks.xml"
-    parse_bank(jp_bank_file, sid_to_ix_ja)
-    return sid_to_ix_cn, sid_to_ix_ja
-
-
-def map_bank_name_to_files(p: Path) -> dict[str, list[Path]]:
-    table = {}
-    for f in p.iterdir():
-        bank_name = f.name.split("-")[0]
-        if bank_name not in table:
-            table[bank_name] = []
-        table[bank_name].append(f)
-    return table
-
-
-def get_text(i18n, v):
-    name_obj = v['VoiceName']
-    key = name_obj.get("Key", None)
-    if key is not None:
-        name_cn = name_obj["SourceString"]
-    else:
-        name_cn = ""
-    name_en = i18n.get(key, "")
-    content_obj = v['Content']
-    key = content_obj.get("Key", None)
-    if key is not None:
-        content_cn = content_obj["SourceString"]
-    else:
-        content_cn = ""
-    content_en = i18n.get(key, "")
-    return content_cn, content_en, name_cn, name_en
-
-
-def in_game_triggers() -> list[Trigger]:
-    i18n = get_game_json()['InGameVoiceTrigger']
-    table = get_table("InGameVoiceTrigger")
-    result: list[Trigger] = []
-    for k, v in table.items():
-        description_cn = v['Desc']['SourceString']
-        description_en = i18n.get(v['Desc']['Key'], "")
-        role_id: int = v['RoleId']
-        voice_id: list[int] = [v['VoiceId']]
-        if "RandomVoiceIds" in v:
-            lst = v["RandomVoiceIds"]
-            if len(lst) > 0:
-                assert v['IsRandom']
-                voice_id = lst
-        result.append(
-            Trigger(k, description_cn=description_cn, description_en=description_en, voice_id=voice_id, role_id=role_id,
-                    type=VoiceType.BATTLE))
-    return result
-
-
-def in_game_triggers_upgrade() -> list[UpgradeTrigger]:
-    table = get_table("InGameVoiceUpgrade")
-    result: list[UpgradeTrigger] = []
-    for k, v in table.items():
-        trigger: int = v["TriggerInGameVoiceId"]
-        voice_id: list[int] = v["RandomVoiceIdList"]
-        skins: list[int] = v["RoleSkinIdList"]
-        result.append(UpgradeTrigger(trigger, voice_id, skins))
-    return result
-
-
-def role_voice() -> dict[int, Voice]:
-    table_cn, table_jp = parse_banks_xml()
-    bank_name_to_files_cn = map_bank_name_to_files(wav_root_cn)
-    bank_name_to_files_jp = map_bank_name_to_files(wav_root_jp)
-    i18n = get_game_json()['RoleVoice']
-    voice_table = get_table("RoleVoice")
-    path_to_voice: dict[str, Voice] = {}
-
-    voices = {}
-    for k, v in voice_table.items():
-        content_cn, content_en, name_cn, name_en = get_text(i18n, v)
-
-        path = v["AkEvent"]["AssetPathName"].split(".")[-1]
-        file_cn = find_audio_file(path, table_cn, bank_name_to_files_cn)
-        if file_cn is None:
-            continue
-        file_jp = find_audio_file(path, table_jp, bank_name_to_files_jp)
-        if file_jp is None:
-            file_jp = ""
-        upgrade = VoiceUpgrade.REGULAR
-        if "org" in path:
-            upgrade = VoiceUpgrade.ORG
-        if "red" in path:
-            upgrade = VoiceUpgrade.RED
-        voice = Voice(id=[k],
-                      role_id=v['RoleId'],
-                      quality=v['Quality'],
-                      name_cn=name_cn.strip(),
-                      name_en=name_en.strip(),
-                      text_cn=content_cn.strip(),
-                      text_en=content_en.strip(),
-                      path=path.strip(),
-                      file_cn=file_cn.strip(),
-                      file_jp=file_jp.strip(),
-                      upgrade=upgrade)
-        if path in path_to_voice:
-            path_to_voice[path].merge(voice)
-            voice = path_to_voice[path]
-        else:
-            path_to_voice[path] = voice
-        voices[k] = voice
-    return voices
-
-
-def match_custom_triggers(voices: list[Voice]) -> list[Trigger]:
-    triggers: dict[str, Trigger] = {}
-
-    def make_trigger(key, name_cn, name_en, type: VoiceType):
-        triggers[key] = Trigger(
-            id=int(key),
-            name_cn=name_cn,
-            name_en=name_en,
-            description_cn=name_cn,
-            description_en=name_en,
-            role_id=0,
-            type=type,
-        )
-
-    for voice_type, table in voice_conversion_table.items():
-        for key, names in table.items():
-            make_trigger(key, names[0], names[1], voice_type)
-
-    voice_found: set[tuple] = set()
-
-    for v in voices:
-        ids = tuple(v.id)
-        if ids in voice_found:
-            continue
-        voice_found.add(ids)
-        digits = re.search(r"(\d{3})(_|$)", v.path)
-        if digits is None:
-            continue
-        digits = digits.group(1)
-        if digits in triggers:
-            triggers[digits].voices.append(v)
-    return list(triggers.values())
 
 
 def pick_two(a: str, b: str) -> str:
@@ -358,7 +95,7 @@ def upload_audio_file(voices: list[Voice], char_name: str):
             upload_audio(path, FilePage(s, "File:" + v.file_page_jp), text)
 
 
-def make_json(triggers: list[Trigger], char_id: int):
+def make_character_json(triggers: list[Trigger], char_id: int):
     def voice_filter(v: Voice):
         return v.role_id == 0 or v.role_id == char_id
 
@@ -370,7 +107,8 @@ def make_json(triggers: list[Trigger], char_id: int):
             if not voice_filter(v):
                 continue
             title_cn = pick_string([t.name_cn, v.name_cn, t.description_cn])
-            obj = {"id": v.id[0], 'title_cn': '', 'title_en': '', '__title_hint': title_cn}
+            title_en = pick_string([t.name_en, v.name_en, t.description_en])
+            obj = {"id": v.id[0], 'title_cn': '', 'title_en': '', '__title_hint': title_cn + "/" + title_en}
             for attribute in attributes:
                 obj[attribute] = getattr(v, attribute)
             result[obj['id']] = obj
@@ -470,7 +208,7 @@ def match_role_voice_with_bwiki(voices: list[Voice]):
                 continue
             digits = digits.group(1)
             upgrade = v.upgrade
-            results = re.findall(rf"语音-{digits}(CN|JP)([^|]+)\|([^|]+)", text, re.DOTALL)
+            results = re.findall(f"语音-{digits}" + r"(CN|JP)([^|]+)\|([^|<}}{{\\]+)", text, re.DOTALL)
             if len(results) == 0:
                 continue
             for r in results:
@@ -478,34 +216,19 @@ def match_role_voice_with_bwiki(voices: list[Voice]):
                     (VoiceUpgrade.RED if "red" in r[1] else VoiceUpgrade.REGULAR)
                 if upgrade != result_upgrade:
                     continue
+                res: str = r[2].strip()
                 if r[0] == "CN":
-                    v.text_cn = r[2].strip()
+                    v.text_cn = res
                 else:
-                    v.text_jp = r[2].strip()
+                    v.text_jp = res
 
 
-def make_character_audio_pages():
+def make_json():
     voices = role_voice()
     match_role_voice_with_bwiki(list(voices.values()))
-    # triggers = dict((t.id, t) for t in in_game_triggers())
-    # upgrades = in_game_triggers_upgrade()
-    # for upgrade in upgrades:
-    #     if upgrade.trigger not in triggers:
-    #         continue
-    #     triggers[upgrade.trigger].children.append(upgrade)
-    custom_triggers = match_custom_triggers(list(voices.values()))
-    all_triggers = custom_triggers
-    result = []
-    for t in all_triggers:
-        for voice_id in t.voice_id:
-            if voice_id not in voices:
-                break
-            t.voices.append(voices[voice_id])
-        else:
-            result.append(t)
+    triggers = match_custom_triggers(list(voices.values()))
     for char_id, char_name in char_id_mapper.items():
-        make_json(result, char_id)
-        make_character_audio_page(char_id)
+        make_character_json(triggers, char_id)
 
 
 def test_audio():
@@ -567,10 +290,63 @@ def test_audio():
                 os.startfile(wav_root_cn / v.file)
 
 
+def make_character_audio_pages():
+    for char_id, char_name in char_id_mapper.items():
+        if char_name == "Fuchsia":
+            make_character_audio_page(char_id)
+
+
+def pull_from_miraheze():
+    pages = PreloadingGenerator(generator=(Page(s, f"{char_name}/audio") for char_name in char_id_mapper.values()))
+    for page in pages:
+        if not page.exists():
+            print(page.title() + " does not exist")
+            continue
+        char_name = page.title().split("/")[0]
+        json_file = Path(f"audio/data/{char_name}.json")
+        assert json_file.exists(), f"{json_file} does not exist"
+        existing = load_json(json_file)
+        path_to_voice: dict[str, dict] = {}
+        for v in existing.values():
+            path_to_voice[v['path']] = v
+        parsed = wtp.parse(page.text)
+        templates = []
+        for t in parsed.templates:
+            if t.name.strip().lower() == "voice/row":
+                templates.append(t)
+        changed = False
+        for t in templates:
+            path = t.get_arg("FileCN").value.replace("CN_", "").replace(".ogg", "").strip()
+            assert path in path_to_voice
+            voice = path_to_voice[path]
+            mapping = {
+                "TextCN": "text_cn",
+                "TextEN": "text_en",
+                "TextJP": "text_jp"
+            }
+            for k, v in mapping.items():
+                arg = t.get_arg(k)
+                if arg is None:
+                    continue
+                arg = arg.value.strip()
+                if arg != "" and arg != voice[v]:
+                    voice[v] = arg
+                    changed = True
+        if not changed:
+            print("No change for " + char_name)
+            continue
+        json.dump(existing, open(json_file, "w", encoding="utf-8"), ensure_ascii=False, indent=4)
+        print("Overwriting " + char_name)
+
+
 def main():
-    make_character_audio_pages()
-    # test_audio()
-    pass
+    commands = {
+        "push": make_character_audio_pages,
+        "gen": make_json,
+        "pull": pull_from_miraheze,
+        "test": test_audio,
+    }
+    commands[argv[1]]()
 
 
 if __name__ == "__main__":
---
+++ audio/audio_parser.py
@@ -0,0 +1,277 @@
+import re
+from dataclasses import dataclass, fields, field
+from enum import Enum
+from pathlib import Path
+
+from audio.data.conversion_table import VoiceType, voice_conversion_table
+from utils.asset_utils import audio_event_root, audio_root, wav_root_cn, wav_root_jp
+from utils.general_utils import load_json, get_game_json, get_table
+
+
+class VoiceUpgrade(Enum):
+    REGULAR = ""
+    ORG = "org"
+    RED = "red"
+
+
+@dataclass
+class Voice:
+    id: list[int]
+    role_id: int = -1
+    quality: int = -1
+    name_cn: str = ""
+    name_en: str = ""
+    text_cn: str = ""
+    text_en: str = ""
+    text_jp: str = ""
+    path: str = ""
+    file_cn: str = ""
+    file_jp: str = ""
+    file_page_cn: str = ""
+    file_page_jp: str = ""
+    upgrade: VoiceUpgrade = VoiceUpgrade.REGULAR
+
+    def merge(self, o: "Voice"):
+        assert self.file_cn == o.file_cn and self.path == o.path
+        assert len(o.id) == 1
+        self.id.append(o.id[0])
+        for f in fields(self):
+            if f.type == str:
+                a1 = getattr(self, f.name)
+                a2 = getattr(o, f.name)
+                if a1 == a2:
+                    continue
+                if a1 == "":
+                    setattr(self, f.name, a2)
+                raise RuntimeError(str(self) + "\n" + str(o))
+        if self.role_id != o.role_id:
+            if self.role_id == 999:
+                self.role_id = o.role_id
+
+
+@dataclass
+class Trigger:
+    id: int
+    type: VoiceType
+    name_cn: str = ""
+    name_en: str = ""
+    description_cn: str = ""
+    description_en: str = ""
+    voice_id: list[int] = field(default_factory=list)
+    # 0: applicable for all
+    # otherwise: applicable to a single character
+    role_id: int = 0
+    voices: list[Voice] = field(default_factory=list)
+    children: list["UpgradeTrigger"] = field(default_factory=list)
+
+    def merge(self, o: "Trigger"):
+        assert self.id == o.id and self.type == o.type
+        for f in fields(self):
+            if f.type == str:
+                a1 = getattr(self, f.name)
+                a2 = getattr(o, f.name)
+                if a1 == a2:
+                    continue
+                if a1 == "":
+                    setattr(self, f.name, a2)
+                raise RuntimeError(str(self) + "\n" + str(o))
+        if self.role_id != o.role_id:
+            if self.role_id == 999:
+                self.role_id = o.role_id
+
+
+@dataclass
+class UpgradeTrigger:
+    trigger: int
+    voice_id: list[int]
+    skins: list[int]
+
+
+def find_audio_file(file_name: str, table: dict, bank_name_to_files: dict) -> str | None:
+    assert len(table) > 0
+    event_file = audio_event_root / f"{file_name}.json"
+    if not event_file.exists():
+        # print(event_file.name + " does not exist")
+        return None
+    json_data = load_json(event_file)
+    if "Properties" not in json_data:
+        json_data = json_data[0]
+    data = json_data["Properties"]
+    bank_name = data["RequiredBank"]["ObjectName"].split("'")[1]
+    short_id = str(data["ShortID"])
+    if short_id not in table:
+        # print(f"Short ID {short_id} is not in conversion table")
+        return None
+    ix = int(table[short_id])
+    candidates = []
+    if bank_name not in bank_name_to_files:
+        # print(f"No file corresponding to bank name " + bank_name)
+        return None
+    for f in bank_name_to_files[bank_name]:
+        if f"-{ix:04d}-" in f.name:
+            candidates.append(f)
+    if len(candidates) == 0:
+        # print(f"No audio file found for {file_name} and bank {bank_name}")
+        return None
+    return candidates[0].name
+
+
+def parse_bank(bank_file: Path, table: dict[str, str]):
+    assert bank_file.exists()
+    lines = open(bank_file, "r", encoding="utf-8").readlines()
+    ix = None
+    sid = None
+    for line in lines:
+        if "ix=" in line:
+            ix = re.search(r'ix="(\d+)"', line).group(1)
+        if 'ty="sid"' in line:
+            sid = re.search(r'va="(\d+)"', line).group(1)
+            if ix is not None:
+                table[sid] = ix
+
+
+def parse_banks_xml():
+    sid_to_ix_cn = {}
+    cn_bank_file = audio_root / "banks/cn_banks.xml"
+    parse_bank(cn_bank_file, sid_to_ix_cn)
+    sid_to_ix_ja = {}
+    jp_bank_file = audio_root / "banks/ja_banks.xml"
+    parse_bank(jp_bank_file, sid_to_ix_ja)
+    return sid_to_ix_cn, sid_to_ix_ja
+
+
+def map_bank_name_to_files(p: Path) -> dict[str, list[Path]]:
+    table = {}
+    for f in p.iterdir():
+        bank_name = f.name.split("-")[0]
+        if bank_name not in table:
+            table[bank_name] = []
+        table[bank_name].append(f)
+    return table
+
+
+def get_text(i18n, v):
+    name_obj = v['VoiceName']
+    key = name_obj.get("Key", None)
+    if key is not None:
+        name_cn = name_obj["SourceString"]
+    else:
+        name_cn = ""
+    name_en = i18n.get(key, "")
+    content_obj = v['Content']
+    key = content_obj.get("Key", None)
+    if key is not None:
+        content_cn = content_obj["SourceString"]
+    else:
+        content_cn = ""
+    content_en = i18n.get(key, "")
+    return content_cn, content_en, name_cn, name_en
+
+
+def in_game_triggers() -> list[Trigger]:
+    i18n = get_game_json()['InGameVoiceTrigger']
+    table = get_table("InGameVoiceTrigger")
+    result: list[Trigger] = []
+    for k, v in table.items():
+        description_cn = v['Desc']['SourceString']
+        description_en = i18n.get(v['Desc']['Key'], "")
+        role_id: int = v['RoleId']
+        voice_id: list[int] = [v['VoiceId']]
+        if "RandomVoiceIds" in v:
+            lst = v["RandomVoiceIds"]
+            if len(lst) > 0:
+                assert v['IsRandom']
+                voice_id = lst
+        result.append(
+            Trigger(k, description_cn=description_cn, description_en=description_en, voice_id=voice_id, role_id=role_id,
+                    type=VoiceType.BATTLE))
+    return result
+
+
+def in_game_triggers_upgrade() -> list[UpgradeTrigger]:
+    table = get_table("InGameVoiceUpgrade")
+    result: list[UpgradeTrigger] = []
+    for k, v in table.items():
+        trigger: int = v["TriggerInGameVoiceId"]
+        voice_id: list[int] = v["RandomVoiceIdList"]
+        skins: list[int] = v["RoleSkinIdList"]
+        result.append(UpgradeTrigger(trigger, voice_id, skins))
+    return result
+
+
+def role_voice() -> dict[int, Voice]:
+    table_cn, table_jp = parse_banks_xml()
+    bank_name_to_files_cn = map_bank_name_to_files(wav_root_cn)
+    bank_name_to_files_jp = map_bank_name_to_files(wav_root_jp)
+    i18n = get_game_json()['RoleVoice']
+    voice_table = get_table("RoleVoice")
+    path_to_voice: dict[str, Voice] = {}
+
+    voices = {}
+    for k, v in voice_table.items():
+        content_cn, content_en, name_cn, name_en = get_text(i18n, v)
+
+        path = v["AkEvent"]["AssetPathName"].split(".")[-1]
+        file_cn = find_audio_file(path, table_cn, bank_name_to_files_cn)
+        if file_cn is None:
+            continue
+        file_jp = find_audio_file(path, table_jp, bank_name_to_files_jp)
+        if file_jp is None:
+            file_jp = ""
+        upgrade = VoiceUpgrade.REGULAR
+        if "org" in path:
+            upgrade = VoiceUpgrade.ORG
+        if "red" in path:
+            upgrade = VoiceUpgrade.RED
+        voice = Voice(id=[k],
+                      role_id=v['RoleId'],
+                      quality=v['Quality'],
+                      name_cn=name_cn.strip(),
+                      name_en=name_en.strip(),
+                      text_cn=content_cn.strip(),
+                      text_en=content_en.strip(),
+                      path=path.strip(),
+                      file_cn=file_cn.strip(),
+                      file_jp=file_jp.strip(),
+                      upgrade=upgrade)
+        if path in path_to_voice:
+            path_to_voice[path].merge(voice)
+            voice = path_to_voice[path]
+        else:
+            path_to_voice[path] = voice
+        voices[k] = voice
+    return voices
+
+
+def match_custom_triggers(voices: list[Voice]) -> list[Trigger]:
+    triggers: dict[str, Trigger] = {}
+
+    def make_trigger(key, name_cn, name_en, type: VoiceType):
+        triggers[key] = Trigger(
+            id=int(key),
+            name_cn=name_cn,
+            name_en=name_en,
+            description_cn=name_cn,
+            description_en=name_en,
+            role_id=0,
+            type=type,
+        )
+
+    for voice_type, table in voice_conversion_table.items():
+        for key, names in table.items():
+            make_trigger(key, names[0], names[1], voice_type)
+
+    voice_found: set[tuple] = set()
+
+    for v in voices:
+        ids = tuple(v.id)
+        if ids in voice_found:
+            continue
+        voice_found.add(ids)
+        digits = re.search(r"(\d{3})(_|$)", v.path)
+        if digits is None:
+            continue
+        digits = digits.group(1)
+        if digits in triggers:
+            triggers[digits].voices.append(v)
+    return list(triggers.values())
