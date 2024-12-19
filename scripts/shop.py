790579acb4f4e6aaf7d5e1a6dc9d5194aa2bebdb
Peter Li <lihaohong6@gmail.com>
2024-12-19 16:47:33 -0500

    Add automatic banner data generation

--- scripts/shop.py
+++ scripts/shop.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
 from datetime import datetime
+from functools import cache
 
+from char_info.gallery import SkinInfo, parse_skin_tables
 from global_config import char_id_mapper
 from page_generator.items import Item, get_all_items
 from utils.general_utils import get_table_global, parse_ticks, get_table, save_json_page
 from utils.json_utils import get_all_game_json
 from utils.lang import ENGLISH, CHINESE
-from utils.lang_utils import get_multilanguage_dict
+from utils.lang_utils import get_multilanguage_dict, compose, StringConverters
 
 
 @dataclass
@@ -33,7 +35,9 @@ def parse_banners(use_cn: bool = False) -> list[Banner]:
     for k, v in table:
         if v['Type'] != 1:
             continue
-        name = get_multilanguage_dict(i18n, f"{k}_Name", extra=v['Name']['SourceString'])
+        name = get_multilanguage_dict(i18n, f"{k}_Name", extra=v['Name']['SourceString'],
+                                      converter=compose(StringConverters.basic_converter,
+                                                        StringConverters.all_caps_remove))
         if len(name) == 0:
             continue
 
@@ -45,9 +49,45 @@ def parse_banners(use_cn: bool = False) -> list[Banner]:
     return result
 
 
-def generate_gacha_page():
-    banners = parse_banners(use_cn=True)
-    print("\n".join(map(str, banners)))
+@cache
+def reverse_skin_lookup_table() -> dict[int, str]:
+    skins = parse_skin_tables()
+    result = {}
+    for char_name, skin_list in skins.items():
+        for skin in skin_list:
+            for sid in skin.id:
+                result[sid] = char_name
+    return result
+
+
+def get_role_legendary_skin(banner_id: int) -> SkinInfo:
+    for drop in parse_gacha_drops()[banner_id]:
+        if drop.item.quality == 5 and isinstance(drop.item, SkinInfo):
+            role_name = reverse_skin_lookup_table()[drop.item.id[0]]
+            return drop.item.get_mh_portrait_title(role_name)
+    raise RuntimeError(f"No legendary skin found for banner {banner_id}")
+
+
+def banners_to_serializable(banners: list[Banner]) -> list[dict]:
+    result = []
+    for b in banners:
+        d = {
+            "name": b.name,
+            "start": str(b.start),
+            "end": str(b.end),
+            "image": get_role_legendary_skin(b.group),
+        }
+        result.append(d)
+    return result
+
+
+def make_gacha_banners():
+    banners = parse_banners(use_cn=False)
+    banners = [b for b in banners
+               if b.end.year >= 2024]
+    banners.sort(key=lambda b: b.start)
+    obj = banners_to_serializable(banners)
+    save_json_page("Module:Gacha/banners.json", obj)
 
 
 @dataclass
@@ -82,8 +122,7 @@ def parse_gacha_drops(use_cn: bool = False) -> dict[int, list[GachaDrop]]:
     return result
 
 
-def save_gacha_page_json(use_cn: bool = False):
-    banners = parse_banners(use_cn)
+def save_gacha_drop_json(use_cn: bool = False):
     all_drops = parse_gacha_drops(use_cn)
     result = {}
     for group_id in all_drops:
@@ -92,14 +131,14 @@ def save_gacha_page_json(use_cn: bool = False):
     save_json_page(page_name, result)
 
 
-def generate_gacha_data():
-    save_gacha_page_json(use_cn=False)
-    save_gacha_page_json(use_cn=True)
+def make_gacha_drop_data():
+    save_gacha_drop_json(use_cn=False)
+    save_gacha_drop_json(use_cn=True)
 
 
 def main():
-    generate_gacha_data()
-    generate_gacha_page()
+    # make_gacha_drop_data()
+    make_gacha_banners()
 
 
 if __name__ == '__main__':
