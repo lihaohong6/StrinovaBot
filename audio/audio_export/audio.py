4daf5ba8bf37ae5f181c01006912316d2585ce43
Peter Li <lihaohong6@gmail.com>
2024-12-30 05:51:05 -0500

    Fix issue with multiple bnk file export in wwiser. Semi-working parallel audio export script

--- audio/audio_export/audio.py
+++ audio/audio_export/audio.py
@@ -7,6 +7,8 @@ import subprocess
 import sys
 from dataclasses import dataclass
 from pathlib import Path
+from queue import Queue
+from subprocess import Popen
 
 sys.path.append("../..")
 
@@ -18,6 +20,28 @@ class AudioConfig:
     audio_path: Path
     bank_file: str
     output_dir: str
+
+
+class SimpleProcessPool:
+    SIZE_LIMIT = 128
+
+    def __init__(self):
+        self.pool: Queue[Popen] = Queue()
+        self.size = 0
+
+    def reap(self, target: int = 0):
+        while self.size > target:
+            process = self.pool.get()
+            process.wait()
+
+
+    def submit(self, commands: list, *args, **kwargs):
+        self.pool.put(subprocess.Popen(commands, *args, **kwargs))
+        self.size += 1
+        if self.size > self.SIZE_LIMIT:
+            self.reap(self.SIZE_LIMIT)
+
+
 
 
 wwiser_location = Path('wwiser.pyz')
@@ -33,16 +57,18 @@ def get_configs(root: Path, root_gl: Path):
 
 
 def txtp_to_wav(source: Path, dest: Path):
-    
     dest.mkdir(exist_ok=True, parents=True)
+    processes = []
     for file in source.rglob("*.txtp"):
         file_name = file.name
         out_path = dest.joinpath(file_name.replace(".txtp", ".wav"))
-        def func(txtp_file, output_path):
-            subprocess.run(["vgmstream-cli", txtp_file, "-o", output_path.absolute()],
-                             stdout=open(os.devnull, 'wb'),
-                             cwd=source.parent)
-        func(file, out_path)
+        processes.append(Popen(
+            ["vgmstream-cli", file, "-o", out_path.absolute()],
+            stdout=open(os.devnull, 'wb'),
+            cwd=source.parent
+        ))
+    for p in processes:
+        p.wait()
 
 
 def make_bank_file(banks_dir, config):
@@ -61,25 +87,20 @@ def make_bank_file(banks_dir, config):
 
 
 def make_txtp_files(audio_dir, txtp):
-    config_file = Path(
-        ''.join(random.choices(string.ascii_uppercase + string.digits, k=15)) + "wwconfig.txt").absolute()
-    with open(config_file, "w") as f:
-        f.write("-g -go ")
-        f.write(f'"txtp" ')
-        f.write(" ".join(f'"{str(p.relative_to(audio_dir))}"' for p in Path(audio_dir).glob('*.bnk')))
-    subprocess.run(['python', wwiser_location.absolute(), config_file],
-                   cwd=audio_dir)
-    config_file.unlink()
+    processes = []
+    for p in Path(audio_dir).glob('*.bnk'):
+        processes.append(subprocess.Popen(
+            ['python', wwiser_location.absolute(), "-g", "-go", "txtp", str(p.relative_to(audio_dir))],
+            cwd=audio_dir))
+    for p in processes:
+        p.wait()
 
 
 def generate_wav(audio_dir: Path, output_dir: Path):
     txtp = Path(audio_dir.absolute() / 'txtp')
-
     if txtp.exists():
-        shutil.rmtree(txtp)
-
+        shutil.rmtree(txtp, ignore_errors=True)
     make_txtp_files(audio_dir, txtp)
-
     txtp_to_wav(txtp, output_dir)
 
 
@@ -95,14 +116,13 @@ def main():
     print("Audio root " + str(audio_root))
     print("Audio root en " + str(audio_root_en))
 
+    configs = get_configs(audio_root, audio_root_en)
+
+    # Process bnk files
     banks_dir = Path('banks')
     if banks_dir.exists():
         shutil.rmtree(banks_dir)
     banks_dir.mkdir(exist_ok=True)
-
-    configs = get_configs(audio_root, audio_root_en)
-    
-    # # Process bnk files
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for config in configs:
             executor.submit(make_bank_file,
@@ -120,8 +140,7 @@ def main():
             executor.submit(generate_wav, 
                             config.audio_path, Path(config.output_dir))
 
-
 if __name__ == "__main__":
     main()
-else:
+elif __name__ != "__mp_main__":
     raise ImportError("Do not import this module. Just run it.")
